b04f523e561a9824f5681daaeb16e03db1e5a026
jeremie passerat <jeremie.passerat@gmail.com>
2021-01-27 16:21:56 +0100

    Premier Commit

---
+++ src/test/java/fr/univ/orleanstours/dill/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univ.orleanstours.dill.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
